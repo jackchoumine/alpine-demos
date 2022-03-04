ee971b0dfcd8ee696455d5d6015cea4bdf49b78c
jack <jackzhoumine@gmail.com>
2022-03-05 00:25:38 +0800

    docs: 添加文档

--- src/components/MyInput.js
+++ src/components/MyInput.js
@@ -2,11 +2,16 @@
  * @Description :
  * @Date        : 2022-03-04 23:40:51 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-03-05 00:05:34 +0800
+ * @LastEditTime: 2022-03-05 00:17:29 +0800
  * @LastEditors : JackChou
  */
 export const myInputHtml = /*html*/ `
-<div x-data="myInput('hello')">
+<div x-data="myInput('hello')" x-effect="changeValue">
+  <input x-model="value"/>
+  <span x-text="value"></span>
+</div>
+<div>
+  <div x-data="myInput">
   <input x-model="value"/>
   <span x-text="value"></span>
 </div>
@@ -15,8 +20,11 @@ export const myInput = (initValue = 100) => ({
   value: initValue,
   init() {
     console.log('myInput')
-    this.$watch('value', (newValue) => {
-      console.log(newValue)
+    this.$watch('value', (newValue, oldValue) => {
+      console.log(oldValue, '--->', newValue)
     })
+  },
+  changeValue() {
+    console.log(this.value)
   },
 })
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Alpine from 'alpinejs'
 import './assets/style.css'
 import { myInput, myInputHtml } from './components/MyInput'
 
+// 注册组件到页面上
 document.querySelector('#app').innerHTML = myInputHtml
 
 window.Alpine = Alpine
 // @ts-ignore
-Alpine.data('myInput', myInput)
+Alpine.data('myInput', myInput) // 注册数据
 
 queueMicrotask(() => {
+  // NOTE 微任务，确保 Alpine 初始化时，所有的组件都已经注册到页面上
   Alpine.start()
 })
